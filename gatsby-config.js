c04d1b4eb558982ce477db4173d15c3aa0cbce37
Fluffy Tekapo <fluffytekapo@gmail.com>
2021-03-11 14:06:44 +1300

    Added more shopify access

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,11 +50,54 @@ module.exports = {
         // See: https://help.shopify.com/api/custom-storefronts/storefront-api/getting-started#authentication
         accessToken: 'cd2b00045494d33b6f54515c296e0228',
 
+        // Set the API version you want to use. For a list of available API versions,
+        // see: https://help.shopify.com/en/api/storefront-api/reference/queryroot
+        // Defaults to 2019-07
+        apiVersion: "2020-01",
+
         // Set verbose to true to display a verbose output on `npm run develop`
         // or `npm run build`. This prints which nodes are being fetched and how
         // much time was required to fetch and process the data.
         // Defaults to true.
         verbose: true,
+
+        // Number of records to fetch on each request when building the cache
+        // at startup. If your application encounters timeout errors during
+        // startup, try decreasing this number.
+        paginationSize: 250,
+
+        // List of collections you want to fetch.
+        // Possible values are: 'shop' and 'content'.
+        // Defaults to ['shop', 'content'].
+        includeCollections: ["shop", "content"],
+        // Download Images Locally
+        // set to false if you plan on using shopify's CDN
+        downloadImages: true,
+
+        // Allow overriding the default queries
+        // This allows you to include/exclude extra fields when sourcing nodes
+        // Available keys are: articles, blogs, collections, products, shopPolicies, and pages
+        // Queries need to accept arguments for first and after
+        // You will need to include all the fields you want available for a
+        // specific key. View the `shopifyQueries Defaults` section below for a
+        // full list of keys and fields.
+        shopifyQueries: {
+          products: `
+          query GetProducts($first: Int!, $after: String) {
+            products(first: $first, after: $after) {
+              pageInfo {
+                hasNextPage
+              }
+              edges {
+                cursor
+                node {
+                  availableForSale
+                }
+              }
+            }
+          }
+        `,
+        },
       },
     },
     {
